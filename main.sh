d5935947f7435e84afb078630a170e06cc28bac8
ehau417 <131351671+ehau417@users.noreply.github.com>
2023-04-20 12:36:15 +0300

    Create main.sh
    example of an Ubuntu shell script that generates a random port number between 2000-65000 using the shuf command

---
+++ main.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Generate a list of port numbers from 2000-65000
+ports=$(seq 2000 65000)
+
+# Shuffle the list using the shuf command
+shuffled=$(printf '%s\n' "${ports[@]}" | shuf)
+
+# Select the first element of the shuffled list
+port=$(echo "$shuffled" | head -n 1)
+
